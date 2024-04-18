d2edf0db9270f4eeae4af709c5e91f9472d22eaa
ShubhamJoshi-1995 <shuvijoshi@gmail.com>
2024-04-18 08:30:04 +0530

    Added Sum  in java Program

--- src/JavaProgramPractise/HelloWorld.java
+++ src/JavaProgramPractise/HelloWorld.java
@@ -2,6 +2,11 @@ package JavaProgramPractise;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a+b;
+        System.out.println("Sum of Two Number is -> " + sum);
         System.out.println("Hello World");
+
     }
 }
