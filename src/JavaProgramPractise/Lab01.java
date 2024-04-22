b8c2a576cd08de45b5a84cb1d6fd71aa66ec4caa
ShubhamJoshi-1995 <shuvijoshi@gmail.com>
2024-04-22 14:27:41 +0530

    Code Update with proper format

---
+++ src/JavaProgramPractise/Lab01.java
@@ -0,0 +1,28 @@
+package JavaProgramPractise;
+
+public class Lab01 {
+    public static void main(String[] args) {
+
+        byte b = 10;
+        short s = 12;
+        int i = 14;
+        long  l =  9876543210l ;
+        float f = 18f;
+        double d = 20;
+
+        char c = 'c';
+        Boolean boo =true;
+        String st =  "Shubham";
+
+        System.out.println("Values of Byte :"+ b);
+        System.out.println("Values of short :"+ s);
+        System.out.println("Values of int :"+ i);
+        System.out.println("Values of long :"+ l);
+        System.out.println("Values of float :"+ f);
+        System.out.println("Values of double :"+ d);
+        System.out.println("Values of char :"+ c);
+        System.out.println("Values of boolean :"+ boo);
+        System.out.println("Values of String :"+ st);
+
+    }
+}
